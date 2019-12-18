85e06e31f744a892e143202f4cb445a85e9be15e
yinman <15211365310@163.com>
2019-12-18 09:58:07 +0800

    初始化

---
+++ src/main/java/com/mantou/design/MantouDesignApplication.java
@@ -0,0 +1,18 @@
+package com.mantou.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author mantou
+ */
+@SpringBootApplication
+public class MantouDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MantouDesignApplication.class, args);
+    }
+
+}
